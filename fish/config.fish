979c4644194c6028cc036fa44b55ac2a2f0f5551
Jossey <Jossey @ Archtop>
2026-07-15 11:03:00 -0500

    more QOL life stuff for my laptop

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,54 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    zoxide init fish --cmd cd | source
+  # Commands to run in interactive sessions can go here
+
+  set -gx GTK_THEME Adwaita:dark
+  set -gx QT_STYLE_OVERRIDE Adwaita-Dark
+  set -gx QT_QPA_PLATFORMTHEME qt6ct
+
+  echo "source /usr/share/doc/pkgfile/command-not-found.fish" >> ~/.config/fish/config.fish
+
+  zoxide init fish --cmd cd | source
 end
+
+fish_add_path /home/irene/.spicetify
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
+source /usr/share/doc/pkgfile/command-not-found.fish
